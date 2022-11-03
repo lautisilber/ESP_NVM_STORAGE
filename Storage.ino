f6f38263ddbd62ce01c60e174323c2e71d60e742
lautisilber <lautisilbergleit@gmail.com>
2022-11-03 19:08:45 -0300

    Accept several variables in NVM. Distribution has to be stated at run time tho

--- Storage.ino
+++ Storage.ino
@@ -7,27 +7,45 @@ typedef struct A {
     char c[8];
 };
 
+typedef struct B {
+    char a[32];
+    double b;
+};
+
 void write() {
     A a = {8, 13.96};
     strlcpy(a.c, "holo", 8);
+
+    B b;
+    strlcpy(b.a, "chausitisimo", 32);
+    b.b = 1654.324;
     
-    StorageNVM.put(a);
+    StorageNVM.put(0, a);
+    StorageNVM.put(1, b);
 }
 
 void read() {
     A a;
+    B b;
     
-    StorageNVM.get(a);
+    StorageNVM.get(0, a);
+    StorageNVM.get(1, b);
     
     Serial.println(a.a);
     Serial.println(a.b);
     Serial.println(a.c);
+    Serial.println();
+    Serial.println(b.a);
+    Serial.println(b.b);
 }
 
 void setup() {
     Serial.begin(115200);
     Serial.println("\n");
     delay(1000);
+
+    StorageNVM.addToScheme<A>();
+    StorageNVM.addToScheme<B>();
 
     write();
     Serial.println("write done");
--- StorageNVM.hpp
+++ StorageNVM.hpp
@@ -3,32 +3,51 @@
 
 #include <Arduino.h>
 #include <EEPROM.h>
+#include <vector>
 
 #define STORAGE_SIZE 512
+
+
+struct StorageMetadata {
+    StorageMetadata(size_t _size, size_t _trailingBytes) {
+        size = _size;
+        trailingBytes = _trailingBytes;
+    }
+
+    size_t size;
+    size_t trailingBytes;
+};
 
 
 class StorageClass
 {
 private:
     uint8_t _data[STORAGE_SIZE]{0};
+    std::vector<StorageMetadata> _meta;
 
 public:
     StorageClass();
     ~StorageClass();
 
     template<typename T>
-    T &get(T &v) const;
+    T &get(int address, T &v) const;
 
     template<typename T>
-    bool put(const T &v, bool commit=true);
+    bool put(int address, const T &v, bool commit=true);
 
     bool clear();
 
     void PrintAll();
 
+    template <typename T>
+    int addToScheme();
+
 private:
     inline void _load();
+    inline bool _validAddress(int address) const;
     inline bool _commit();
+    inline bool _commitIndex(int address);
+    inline bool _clearIndex(int address);
 };
 
 
@@ -44,6 +63,47 @@ StorageClass::StorageClass()
 StorageClass::~StorageClass()
 {
     EEPROM.end();
+}
+
+
+inline bool StorageClass::_clearIndex(int address) {
+    if (!_validAddress(address)) return false;
+    for (size_t i = _meta[address].trailingBytes; i < _meta[address].trailingBytes + _meta[address].size; i++) {
+        EEPROM.write(i, 0);
+    }
+    bool res = EEPROM.commit();
+    if (res)
+        memset(_data + _meta[address].trailingBytes, 0, _meta[address].size);
+    return res;
+}
+
+inline bool StorageClass::_commitIndex(int address) {
+    if (!_validAddress(address)) return false;
+    for (size_t i = _meta[address].trailingBytes; i < _meta[address].trailingBytes + _meta[address].size; i++) {
+        EEPROM.write(i, _data[i]);
+    }
+    return EEPROM.commit();
+}
+
+inline bool StorageClass::_validAddress(int address) const {
+    return address >= 0 && address < _meta.size();
+}
+
+template<typename T>
+int StorageClass::addToScheme() {
+    if (_meta.size() == 0) {
+        if (sizeof(T) <= STORAGE_SIZE) {
+            _meta.push_back(StorageMetadata(sizeof(T), 0));
+            return 0;
+        }
+    } else {
+        size_t trailingBytes = _meta[_meta.size()-1].trailingBytes + _meta[_meta.size()-1].size;
+        if (trailingBytes + sizeof(T) <= STORAGE_SIZE) {
+            _meta.push_back(StorageMetadata(sizeof(T), trailingBytes));
+            return _meta.size() - 1;
+        }
+    }
+    return -1;
 }
 
 
@@ -64,21 +124,25 @@ inline bool StorageClass::_commit() {
 
 
 template<typename T>
-T &StorageClass::get(T &v) const {
-    if (sizeof(T) < STORAGE_SIZE) {
-        memcpy((uint8_t *) &v, _data, sizeof(T));
+T &StorageClass::get(int address, T &v) const {
+    if (_validAddress(address)) {
+        if (sizeof(T) == _meta[address].size) {
+            memcpy((uint8_t *) &v, _data + _meta[address].trailingBytes, sizeof(T));
+        }
     }
     return v;
 }
 
 
 template<typename T>
-bool StorageClass::put(const T &v, bool commit) {
-    if (sizeof(T) < STORAGE_SIZE) {
-        memcpy(_data, (const uint8_t *) &v, sizeof(T));
-        if (commit)
-            return _commit();
-        return true;
+bool StorageClass::put(int address, const T &v, bool commit) {
+    if (_validAddress(address)) {
+        if (sizeof(T) == _meta[address].size) {
+            memcpy(_data + _meta[address].trailingBytes, (uint8_t *) &v, sizeof(T));
+            if (commit)
+                return _commitIndex(address);
+            return true;
+        }
     }
     return false;
 }
